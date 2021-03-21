0210f62e64b18e2fec99a2071346dfc86544ce80
murat salımov <m.murat1994@hotmail.com>
2021-03-21 13:00:52 +0300

    20 mart

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -29,7 +30,8 @@ namespace WebAPI.Controllers
         public IActionResult GetAll()
         {
 
-           
+            Thread.Sleep(1000);
+            
             //dependency injektion
             var result = _productService.GetAll();
             if (result.Success)
@@ -44,6 +46,18 @@ namespace WebAPI.Controllers
         {
             var result = _productService.GetById(id);
             
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+        
             if (result.Success)
             {
                 return Ok(result);
